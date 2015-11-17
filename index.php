91cc86d65c8bc823f36cb9885b14f4dd8d76affd
dpoakaspine <dpoakaspine@googlemail.com>
2015-11-17 22:35:12 +0100

    here we go

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name: WP Better Admin
+Plugin URI: http://netzstoffi.de
+Description: A nicer and cleaner WordPress Administration
+Author: Stefan Böttcher
+Version: 1.0
+Author URI: http://netzstoffi.de
+*/
+
+add_action( 'admin_enqueue_scripts', 'add_google_font' );
+
+function add_google_font() {
+  wp_enqueue_style( 'my-google-font', '//fonts.googleapis.com/css?family=Roboto' );
+}
+  
+function my_admin_theme_style() {
+    wp_enqueue_style('my-admin-theme', plugins_url('admin.css', __FILE__));
+}
+add_action('admin_enqueue_scripts', 'my_admin_theme_style',900);
+//add_action('login_enqueue_scripts', 'my_admin_theme_style');
+
+/** Add Shadow to Admin Bar **/
+function admin_shadow() {
+   global $wp_admin_bar;
+   
+   echo '<style>html { background-image: url('.get_background_image().'); }</style>';
+   
+}
+add_action( 'admin_head', 'admin_shadow', 800 );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== WP Better Admin ===
+Donate link: http://netzstoffi.de
+Tags: wordpress, admin, backend, style, design, css
+Requires at least: 3.0.1
+Tested up to: 4.3
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A nicer and cleaner WordPress Administration
+
+== Description ==
+
+A nicer and cleaner WordPress Administration
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `wp_scrolltobottomredirect` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Done.
+
+== Frequently Asked Questions ==
+
+= Can you edit your own style? =
+
+In the future this may be also a setting.
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 0.1 =
+* first version
+
