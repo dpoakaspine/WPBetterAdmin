26c307daa9afeefc4f921b47a1b4118d84f25698
dpoakaspine <dpoakaspine@gmail.com>
2016-02-09 18:18:27 +0100

    add custom background image support

--- index.php
+++ index.php
@@ -9,36 +9,41 @@ Version: 1.0
 Author URI: http://netzstoffi.de
 */
 
+//hide admin bar
 add_filter('show_admin_bar', '__return_false');
 
-add_action( 'admin_enqueue_scripts', 'add_google_font' );
+add_action( 'admin_head', 'zm_customize_admin', 800 );
+add_action( 'after_setup_theme', 'zm_theme_setup' );
+add_action( 'admin_enqueue_scripts', 'zm_add_google_font' );
+add_action('admin_enqueue_scripts', 'zm_admin_theme_style',900);
 
-function add_google_font() {
+function zm_add_google_font() {
   wp_enqueue_style( 'my-google-font', '//fonts.googleapis.com/css?family=Roboto' );
 }
-  
-function my_admin_theme_style() {
-    wp_enqueue_style('my-admin-theme', plugins_url('admin.css', __FILE__));
-}
-add_action('admin_enqueue_scripts', 'my_admin_theme_style',900);
-//add_action('login_enqueue_scripts', 'my_admin_theme_style');
 
-/** Add Shadow to Admin Bar **/
-function admin_shadow() {
+function zm_admin_theme_style() {
+    wp_enqueue_style('zm-admin-theme', plugins_url('admin.css', __FILE__));
+}
+
+//add shadow+background image for admin
+function zm_customize_admin() {
    global $wp_admin_bar;
-   
+
 	$img = plugins_url('wall.jpg', __FILE__);
-	//if(!empty( get_background_image() )) { $img = get_background_image(); }
-	//echo '<style>html { background-image: url('.get_background_image().'); }</style>';
+  $background_image = false;
+
+  //for older wp versions
+  if(function_exists('get_background_image')) { $background_image = get_background_image(); }
+
+  //check if there is actually an image
+  if(!empty( $background_image )) { $img = $background_image; }
+
+  //
 	echo '<style>html { background-image: url('.$img.'); }</style>';
-   
 }
-add_action( 'admin_head', 'admin_shadow', 800 );
 
-
-function custom_theme_setup() {
-	add_theme_support( 'custom-background', $arguments );
+function zm_theme_setup() {
+  add_theme_support( 'custom-background', $arguments );
 }
-add_action( 'after_setup_theme', 'custom_theme_setup' );
 
 ?>
